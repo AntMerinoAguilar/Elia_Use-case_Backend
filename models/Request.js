8de5a9d43be1afa8867422eaf931aa7044bf6434
agasurfer <agasurfer@hotmail.com>
2025-02-05 10:05:51 +0100

    models created

---
+++ models/Request.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+//Model Demande
+
+const requestSchema = new mongoose.Schema({
+  requesterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent', required: true },
+  shiftId: { type: mongoose.Schema.Types.ObjectId, ref: 'Shift', required: true },
+  timeSlot: {
+    startTime: Date,
+    endTime: Date
+  },
+  requestType: { type: String, enum: ['Replacement', 'Swap'], required: true },
+  status: { type: String, enum: ['Pending', 'Approved', 'Rejected'], default: 'Pending' },
+  message: String,
+  targetAgentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Agent', default: null } // Ciblage optionnel
+}, { timestamps: true });
+
+const Request = mongoose.model('Request', requestSchema);
+
+module.exports = Request
---
+++ server.js
@@ -0,0 +1,8 @@
+
+
+
+
+const Agent = require('./models/Agent');
+const Shift = require('./models/Shift');
+const Unavailability = require('./models/Unavailability');
+const Request = require('./models/Request');
