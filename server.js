d7f2ff3efce12249289ebbd8c16b79711db9d635
Antoine Merino Aguilar <antoine.merino@hotmail.com>
2025-02-05 11:03:34 +0100

    Erreur fixé

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connecté"))
   .catch(err => console.error(err));
 
