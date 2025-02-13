99338f7ce39a7f1560737a23ed0b77ec17911972
Antoine Merino Aguilar <antoine.merino@hotmail.com>
2025-02-13 12:05:40 +0100

    Update schema Agent color
    Rajout de la couleur pour les agents dans le schema Agent

--- models/Agent.js
+++ models/Agent.js
@@ -1,23 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Model Agent
 
 const agentSchema = new mongoose.Schema({
-    profile_pic: String,
-    name: {type: String, required: true},
-    surname: {type: String, required: true},
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    code: {type: String, required: true},
-    telephone: String,
-    sector: String,
-    balance: { type: Number, default: 0 },
-    history: [{
-    action: String,
-    date: { type: Date, default: Date.now },
-    details: String
-    }]
+  profile_pic: String,
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  code: { type: String, required: true },
+  telephone: String,
+  sector: String,
+  balance: { type: Number, default: 0 },
+  color: { type: String, default: "#000000" }, // Couleur associée à l'agent (par défaut noir pour éviter d'avoir une valeur undefined si aucune couleur n'est spécifiée)
+  history: [
+    {
+      action: String,
+      date: { type: Date, default: Date.now },
+      details: String,
+    },
+  ],
 });
-const Agent = mongoose.model('Agent', agentSchema);
+
+const Agent = mongoose.model("Agent", agentSchema);
 
 module.exports = Agent;
