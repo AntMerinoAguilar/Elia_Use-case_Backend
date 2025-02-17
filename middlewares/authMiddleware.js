85004f1be82801402ad635eb1a4ce62918332bbc
Antoine Merino Aguilar <antoine.merino@hotmail.com>
2025-02-17 15:39:57 +0100

    modification de ShiftController et authMiddleware
    Modification de ShiftController et authMiddleware pour récupérer les shifts de l'agent connecté

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const Agent = require("../models/Agent");
 
-const requireAuthMiddleware = (req, res, next) => {
+const requireAuthMiddleware = async (req, res, next) => {
   // Récupérer le token à partir des cookies
   const token = req.cookies.token;
 
@@ -11,7 +12,15 @@ const requireAuthMiddleware = (req, res, next) => {
   try {
     // Vérifier et décoder le token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.agent = decoded; // Stocker les infos de l'agent décodé dans la requête
+
+    // Récupérer l'agent complet depuis la base de données
+    const agent = await Agent.findById(decoded.id).select("-password"); // Exclut le mot de passe
+
+    if (!agent) {
+      return res.status(404).json({ message: "Agent non trouvé." });
+    }
+
+    req.agent = agent; // Attacher l'agent complet à la requête
     next();
   } catch (err) {
     res.status(400).json({ message: "Token invalide ou expiré." });
