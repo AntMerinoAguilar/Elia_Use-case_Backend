dbda9485d5004756ca26f2aa3710c267926215a5
agasurfer <agasurfer@hotmail.com>
2025-02-20 11:59:56 +0100

    get login test

--- routes/authenticationsRoutes.js
+++ routes/authenticationsRoutes.js
@@ -5,6 +5,19 @@ const authController = require("../controllers/authController");
 // Route pour la connexion
 router.post("/login", authController.login);
 
+// route get test
+
+router.get("/login", (req, res) => {
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None", // Permet l'accès entre différents ports (5173 → 3000)
+      domain: ".vercel.app",  // Spécifie que le cookie appartient à localhost ou domain
+      /* path: "/", */ // Rend le cookie accessible sur toutes les routes
+      maxAge: 10 * 24 * 60 * 60 * 1000, // 10 jours
+    });
+})
+
 // Route pour la déconnexion
 router.post("/logout", authController.logout);
 
